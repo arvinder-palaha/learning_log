817f1eb01f023a1d32254b728ebfda8254639459
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-17 21:41:49 +0100

    Created an entry form, overriding the defualt text entry widget with a wider one.

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 
-from .models import Topic
+from .models import Topic, Entry
 
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
         labels = {'text': ''} # this tells django not to generate a label for the text field
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': ''}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
