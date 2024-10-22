1f929fab905210030f449a4578fd20e9d359fa87
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-22 23:10:46 +0100

    added url and view for registration of user.

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # include default auth urls
     path('', include('django.contrib.auth.urls')),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,24 @@
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LogoutView
 from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
 
 class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('learning_logs:index')
     template_name = 'registration/logged_out.html'
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect('learning_logs:index')
+
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
